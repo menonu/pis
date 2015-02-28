0781d5088b61282f92f01ad2443e6d0b022e24e3
SIPSA01 <flab@awcc.uec.ac.jp>
2015-02-28 19:02:12 +0900

    iqpower

--- powervstime.py
+++ powervstime.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python
 # coding: utf-8
-import sys,math,struct,os,subprocess,csv,numpy,time,string,itertools
+import sys,math,struct,os,subprocess,csv,numpy,time,string,itertools,argparse
 import datetime
 from signal import signal,SIGPIPE,SIG_DFL
 
 signal(SIGPIPE,SIG_DFL)
 
 class splitter:
-    def __init__(self,readfile):
+    def __init__(self,args):
         self.half = 512
         self.headerlen = 1024
         self.valuelen = 112000000
-        self.r = readfile
-        pass
+        self.inputfile = args.target
+        self.outputdir = args.dir
 
     def run(self):
-        with open(sys.argv[1],'rb') as f:
-#            size = 0
-#            gpsblock = f.read(self.half)
-#            f.seek(-512,1)
-#            headerblock = f.read(self.headerlen)
-#            gpsbuf = struct.unpack('512s',gpsblock)[0]
-#            gpsstr = gpsbuf.rstrip(' \t\r\n\0').split(',')
-#            date = '20'+gpsstr[9][4:]+gpsstr[9][2:4]+gpsstr[9][0:2]+str(int(gpsstr[1][0:2])+9)+gpsstr[1][2:]
-#            fformat = struct.Struct('f').unpack
-            f.seek(1024,1)
+        with open(self.inputfile,'rb') as f:
+            size = 0
+            gpsblock = f.read(self.half)
+            f.seek(-512,1)
+            headerblock = f.read(self.headerlen)
+            gpsbuf = struct.unpack('512s',gpsblock)[0]
+            gpsstr = gpsbuf.rstrip(' \t\r\n\0').split(',')
+            date = '20'+gpsstr[9][4:]+gpsstr[9][2:4]+gpsstr[9][0:2]+str(int(gpsstr[1][0:2])+9)+gpsstr[1][2:]
+            fformat = struct.Struct('f').unpack
+            f.seek(1024)
             body = f.read(self.valuelen)
         nary = numpy.fromstring(body,numpy.float32)
         bnary = nary.reshape(self.valuelen/4/2,2)
@@ -32,14 +32,25 @@ class splitter:
         #numpy.savetxt('iq.txt',bnary,fmt='%2.2e')
         powerary = numpy.power(tnary[0],2)+numpy.power(tnary[1],2)
         dbary = 10*numpy.log10(powerary/50)
-        numpy.savetxt('likedbm.txt',dbary,fmt='%2.2f')
+        numpy.savetxt(self.outputdir + '/' + date + '.txt',dbary,fmt='%2.2f')
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print "Too few args"
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description = 'test')
+    parser.add_argument('target',\
+            action='store',\
+            nargs=None,\
+            default=None,\
+            type=str,\
+            )
+    parser.add_argument('dir',\
+            action='store',\
+            nargs='?',\
+            default='./',\
+            type=str,\
+            )
+    args = parser.parse_args()
     try:
-        m = splitter(sys.argv[1])
+        m = splitter(args)
         m.run()
     except KeyboardInterrupt:
         pass
